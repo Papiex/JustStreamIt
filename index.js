c2bd0a7f22da6e4766716a7910fda7ab9ddaeaff
Papiexx <boutdepapier27@outlook.fr>
2021-11-18 14:21:17 +0100

    Added a function that displays the movie with the highest rating at the top of the site

--- index.js
+++ index.js
@@ -67,3 +67,61 @@ function closeTheModal()
 {
     modal.style.display = "none";
 }
+
+// Functions that display movie(s)
+let principal_url = "http://localhost:8000/api/v1/titles/";
+let best_movie_location = document.getElementsByClassName("box1")[0];
+
+async function getInfoUrl(url)
+// Get data of a single movie
+{
+    let response = await fetch(url);
+    let data = await response.json();
+    return data;
+}
+
+function showInfoUrl(data, insert_in)
+// Show data of a single movie
+{
+    let to_insert = 
+        `<img class="top_film" src="${data.image_url}" alt="">
+          <div class="element_box">
+           <div class="titre_video">${data.title}</div>
+           <div class="play">Play</div>
+           <div class="top_resume">
+            <p>${data.long_description}</p>
+           </div>
+          </div>`;
+    
+    insert_in.innerHTML = to_insert;
+}
+
+async function displayBestMovie()
+// Show the best movie(s) and display one if there are several.
+{
+    let imdb = 10;
+    let value_to_decrement = 0.1;
+    let results = [];
+    let url = principal_url + "?imdb_score_min=" + String(imdb);
+
+    while (results.length === 0) {
+        let result = await getInfoUrl(url);
+        url = principal_url + "?imdb_score_min=" + String(imdb);
+        if(result.count === 0) {
+            imdb -= value_to_decrement;
+        } else {
+            for (let movie of result.results) {
+                results.push(movie)
+            }
+        }
+    }
+    let best_movie_selection = results[Math.floor(Math.random()*results.length)];
+    let movie_data = await getInfoUrl(best_movie_selection.url)
+    showInfoUrl(movie_data, best_movie_location);
+
+
+}
+
+displayBestMovie();
+
+
