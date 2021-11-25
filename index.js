ea8d5e7c6e2437db3a45d4d1bbf8bd6172f5777f
Papiexx <boutdepapier27@outlook.fr>
2021-11-25 13:11:08 +0100

    in development, promise=>(best movies array iteration)

--- index.js
+++ index.js
@@ -1,24 +1,23 @@
 // Slider
-let images = ["1.jpg", "2.jpg", "3.jpg", "4.jpg", "5.jpg", "6.jpg", "7.jpg"];
 let actual_image = 0;
 
-function prev(carousel)
+function prev(carousel, movies_list)
 // Slide previous image on the selected carousel on click
 {
-    if(actual_image <= 0) actual_image = images.length;
+    if(actual_image <= 0) actual_image = 7;
         actual_image--;
-        return setImg(carousel);
+        setImg(carousel, movies_list);
 }
 
-function next(carousel)
+function next(carousel, movies_list)
 // Slide next image on the selected carousel on click
 {
-    if(actual_image >= images.length-1) actual_image = -1;
+    if(actual_image >= 7-1) actual_image = -1;
         actual_image++;
-        return setImg(carousel);
+        setImg(carousel, movies_list)
 }
 
-function setImg(carousel)
+function setImg(carousel, movies_list)
 // Put the four images in the right order when click on next or previous 
 {
     let sliderImage1 = document.querySelector("."+ carousel + " img.cover1");
@@ -27,30 +26,135 @@ function setImg(carousel)
     let sliderImage4 = document.querySelector("."+ carousel + " img.cover4");
 
     if(actual_image == 4){
-        return sliderImage1.setAttribute("src", "images/"+images[actual_image]) +
-        sliderImage2.setAttribute("src", "images/"+images[actual_image+1]) +
-        sliderImage3.setAttribute("src", "images/"+images[actual_image+2]) +
-        sliderImage4.setAttribute("src", "images/"+images[actual_image-4]);
+        sliderImage1.setAttribute("src", movies_list[actual_image].image_url);
+        sliderImage2.setAttribute("src", movies_list[actual_image+1].image_url);
+        sliderImage3.setAttribute("src", movies_list[actual_image+2].image_url);
+        sliderImage4.setAttribute("src", movies_list[actual_image-4].image_url);
 
     } else if(actual_image == 5){
-        return sliderImage1.setAttribute("src", "images/"+images[actual_image]) +
-        sliderImage2.setAttribute("src", "images/"+images[actual_image+1]) +
-        sliderImage3.setAttribute("src", "images/"+images[actual_image-5]) +
-        sliderImage4.setAttribute("src", "images/"+images[actual_image-4]);
+        sliderImage1.setAttribute("src", movies_list[actual_image].image_url);
+        sliderImage2.setAttribute("src", movies_list[actual_image+1].image_url);
+        sliderImage3.setAttribute("src", movies_list[actual_image-5].image_url);
+        sliderImage4.setAttribute("src", movies_list[actual_image-4].image_url);
 
     } else if(actual_image == 6){
-        return sliderImage1.setAttribute("src", "images/"+images[actual_image]) +
-        sliderImage2.setAttribute("src", "images/"+images[actual_image-6]) +
-        sliderImage3.setAttribute("src", "images/"+images[actual_image-5]) +
-        sliderImage4.setAttribute("src", "images/"+images[actual_image-4]);
+        sliderImage1.setAttribute("src", movies_list[actual_image].image_url);
+        sliderImage2.setAttribute("src", movies_list[actual_image-6].image_url);
+        sliderImage3.setAttribute("src", movies_list[actual_image-5].image_url);
+        sliderImage4.setAttribute("src", movies_list[actual_image-4].image_url);
 
     } else {
-        return sliderImage1.setAttribute("src", "images/"+images[actual_image]) +
-        sliderImage2.setAttribute("src", "images/"+images[actual_image+1]) +
-        sliderImage3.setAttribute("src", "images/"+images[actual_image+2]) +
-        sliderImage4.setAttribute("src", "images/"+images[actual_image+3]);
+        sliderImage1.setAttribute("src", movies_list[actual_image].image_url);
+        sliderImage2.setAttribute("src", movies_list[actual_image+1].image_url);
+        sliderImage3.setAttribute("src", movies_list[actual_image+2].image_url);
+        sliderImage4.setAttribute("src", movies_list[actual_image+3].image_url);
     }
 }
+
+// Functions that display movie(s)
+const PRINCIPAL_URL = "http://localhost:8000/api/v1/titles/";
+let best_movie_location = document.getElementsByClassName("box1")[0];
+let best_carousel_movies_location = document.getElementsByClassName("carousel1")[0];
+
+async function getInfoUrl(url)
+// Get json data of request
+{
+    let response = await fetch(url);
+    let data = await response.json();
+    return data;
+}
+
+function showDataBestMovie(data, insert_in)
+// Show data of a single movie
+{
+    let to_insert = 
+        `<img class="top_film" src="${data.image_url}" alt="">
+          <div class="element_box">
+           <div class="titre_video">${data.title}</div>
+           <div class="play">Play</div>
+           <div class="top_resume">
+            <p>${data.long_description}</p>
+           </div>
+          </div>`;
+    
+    insert_in.innerHTML = to_insert;
+}
+
+async function getBestMovies()
+// Show the seventh best movies
+{
+    let imdb_min = 10;
+    let value_to_decrement = 0.1;
+    let results = [];
+    let url = PRINCIPAL_URL + "?imdb_score_min=" + String(imdb_min);
+    let search_result = await getInfoUrl(url);
+
+    while (search_result.count <= 6) {
+        imdb_min -= value_to_decrement;
+        url = PRINCIPAL_URL + "?imdb_score_min=" + String(imdb_min);
+        search_result = await getInfoUrl(url);
+    }
+
+    for (let result of search_result.results) {
+        results.push(result)
+    }
+
+    if (search_result.count > 5) {
+        let url_next_page = search_result.next;
+        search_result = await getInfoUrl(url_next_page)
+        for (let result of search_result.results) {
+            results.push(result)
+        }
+    }
+
+    results.sort((a, b) => b.imdb_score-a.imdb_score);
+    return results;
+}
+
+const bestMovies = async () => {
+    const BEST_MOVIES_RESULT = await getBestMovies();
+    createImgElements(best_carousel_movies_location, BEST_MOVIES_RESULT);
+    console.log(BEST_MOVIES_RESULT)
+}
+
+
+async function createImgElements(location, movies_list)
+// Create four div with "element" class at the location
+{
+    let div = document.createElement("div");
+    div.classList.add("element");
+    let button_prev = document.createElement("button");
+    button_prev.classList.add("buttonprev");
+    button_prev.setAttribute("onclick", `prev(${location})`);
+    let img = document.createElement("img")
+    img.setAttribute("src", "images/leftarrow.png");
+    button_prev.appendChild(img);
+    div.appendChild(button_prev);
+    location.appendChild(div);
+
+    for (let step = 0; step < 4; step++) {
+        let div = document.createElement("div");
+        div.classList.add("element");
+        let img = document.createElement("img");
+        img.classList.add("cover" + String(step + 1));
+        img.setAttribute("onclick", "openTheModal()");
+        img.setAttribute("src", `${movies_list[step].image_url}`)
+        div.appendChild(img);
+        location.appendChild(div);
+    }
+
+    div = document.createElement("div");
+    div.classList.add("element");
+    let button_next = document.createElement("button");
+    button_next.classList.add("buttonnext");
+    button_next.setAttribute("onclick", `next(${location})`);
+    img = document.createElement("img")
+    img.setAttribute("src", "images/rightarrow.png");
+    button_next.appendChild(img);
+    div.appendChild(button_next);
+    location.appendChild(div);
+}
+
 
 //Fênetre modale
 let modal = document.getElementById("my_modal");
@@ -68,63 +172,4 @@ function closeTheModal()
     modal.style.display = "none";
 }
 
-// Functions that display movie(s)
-let principal_url = "http://localhost:8000/api/v1/titles/";
-let best_movie_location = document.getElementsByClassName("box1")[0];
-
-async function getInfoUrl(url)
-// Get data of a single movie
-{
-    let response = await fetch(url);
-    let data = await response.json();
-    return data;
-}
-
-function showInfoUrl(data, insert_in)
-// Show data of a single movie
-{
-    let to_insert = 
-        `<img class="top_film" src="${data.image_url}" alt="">
-          <div class="element_box">
-           <div class="titre_video">${data.title}</div>
-           <div class="play">Play</div>
-           <div class="top_resume">
-            <p>${data.long_description}</p>
-           </div>
-          </div>`;
-    
-    insert_in.innerHTML = to_insert;
-}
-
-async function displayBestMovie()
-// Show the best movie(s) and display one if there are several.
-{
-    let imdb = 10;
-    let value_to_decrement = 0.1;
-    let results = [];
-    let url = principal_url + "?imdb_score_min=" + String(imdb);
-
-    while (results.length === 0) {
-        let result = await getInfoUrl(url);
-        url = principal_url + "?imdb_score_min=" + String(imdb);
-        if(result.count === 0) {
-            imdb -= value_to_decrement;
-        } else {
-            for (let movie of result.results) {
-                results.push(movie)
-            }
-        }
-    }
-    let best_movie_selection = results[Math.floor(Math.random()*results.length)];
-    let movie_data = await getInfoUrl(best_movie_selection.url)
-    showInfoUrl(movie_data, best_movie_location);
-
-
-}
-
-displayBestMovie();
-
-displayBestMoviesCarousel()
-
-displayBestMoviesAction()
-
+bestMovies();
